c6a37cf62ee6d4885ea5c326bea03c1f7cac1227
Rakesh <114543952+rakesh-th@users.noreply.github.com>
2022-11-30 16:50:51 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import  LabelEncoder
-import xgboost as xgb
+from xgboost import XGBClassifier
 import numpy as np
 st.header("Credit Card Approval Prediction")
 st.text_input("Enter your Name: ", key="name")
 my_data = pd.read_csv("final_application.csv")
-data = pd.read_csv("https://raw.githubusercontent.com/gurokeretcha/WishWeightPredictionApplication/master/Fish.csv")
-#load label encoder
-encoder = LabelEncoder()
-encoder.classes_ = np.load('classes.npy',allow_pickle=True)
 
 # load model
-best_xgboost_model = xgb.XGBRegressor()
+best_xgboost_model = XGBClassifier()
 best_xgboost_model.load_model("best_model.json")
 
 if st.checkbox('Show Training Dataframe'):
@@ -37,6 +33,12 @@ with left_column:
     inp_Realty = st.radio(
         'Does he own any Realty(properties):',
         np.unique(my_data['Realty']))
+
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Family_Status = st.radio(
+        'Does he own any Realty(properties):',
+        np.unique(my_data['Family_Status']))
 
 left_column, right_column = st.columns(2)
 with left_column:
@@ -66,12 +68,11 @@ input_ACCOUNT_DURATION = st.slider('Account Duration in Months:', 0, max(my_data
 
 
 if st.button('Make Prediction'):
-    input_species = encoder.transform(np.expand_dims(inp_species, -1))
-    inputs = np.expand_dims(
-        [int(input_species), input_Length1, input_Length2, input_Length3, input_Height, input_Width], 0)
+    inputs = [int(123), inp_Gender, inp_Car, inp_Realty, input_Children, input_Salary, inp_Income_Type, inp_Education, inp_Family_Status, inp_House_Type, input_AGE, input_EXPERIENCE, input_Family_Size, input_ACCOUNT_DURATION]
     prediction = best_xgboost_model.predict(inputs)
-    print("final pred", np.squeeze(prediction, -1))
-    st.write(f"Your fish weight is: {np.squeeze(prediction, -1):.2f}g")
+    if prediction:
+        st.write("Your Credit Card is Declined")
+    else:
+        st.write("Your Credit Card is Approved")
 
     st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
-    st.write(f"If you want to see more advanced applications you can follow me on [medium](https://medium.com/@gkeretchashvili)")
