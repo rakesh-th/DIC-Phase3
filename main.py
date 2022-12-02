594b16d9f95aa94a07d7b5281ac1154937b7ee46
Rakesh <114543952+rakesh-th@users.noreply.github.com>
2022-12-02 00:20:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,64 +14,7 @@ from sklearn.preprocessing import LabelEncoder
 st.header("Credit Card Approval Prediction")
 st.text_input("Enter your Name: ", key="name")
 my_data = pd.read_csv("application.csv")
-
-if st.checkbox('Show Training Dataframe'):
-    my_data
-
-st.subheader("Please provide details of your application!")
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_Gender = st.radio(
-        'Gender of the applicant:',
-        np.unique(my_data['Gender']))
- 
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_Car = st.radio(
-        'Does the applicant own a car:',
-        np.unique(my_data['Car']))
-
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_Realty = st.radio(
-        'Does the applicant own any Realty(properties):',
-        np.unique(my_data['Realty']))
-
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_Family_Status = st.radio(
-        'Is applicant Single/Married:',
-        np.unique(my_data['Family_Status']))
-
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_Income_Type = st.radio(
-        "What is the applicant's source of Income:",
-        np.unique(my_data['Income_Type']))   
-
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_House_Type = st.radio(
-        "What is the applicant'type of House",
-        np.unique(my_data['House_Type']))
-
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_Education = st.radio(
-        'Education of the Applicant',
-        np.unique(my_data['Education']))  
-
-input_Children = st.slider('Number of Children:', 0, max(my_data["Children"]), 3)
-input_Family_Size = st.slider('Family Size:', 0, max(my_data["Family_Size"]), 4)
-input_Salary = st.slider('Salary of the Applicant:', 0.0, max(my_data["Salary"]), 85000.0)
-input_AGE = st.slider('Age of the applicant in Years:', 0.0, max(my_data["AGE"]), 22.8)
-input_EXPERIENCE = st.slider('Experience of the applicant in Years:', 0.0, max(my_data["EXPERIENCE"]), 7.5)
-input_ACCOUNT_DURATION = st.slider('Account Duration with the bank in Months:', 0, max(my_data["ACCOUNT_DURATION"]), 18)
-
-
-# load model
-#best_xgboost_model = XGBClassifier()
-#best_xgboost_model.load_model("best_model.json")
+copy_data = my_data.copy()
 
 le1=LabelEncoder()
 le2=LabelEncoder()
@@ -100,6 +43,65 @@ X_test = scaler.transform(X_test)
 
 best_xgboost_model = XGBClassifier(max_depth=5,n_estimators=250, min_child_weight=8)
 best_xgboost_model.fit(X_train, y_train)
+
+if st.checkbox('Show Training Dataframe'):
+    copy_data
+
+st.subheader("Please provide details of your application!")
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Gender = st.radio(
+        'Gender of the applicant:',
+        np.unique(copy_data['Gender']))
+ 
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Car = st.radio(
+        'Does the applicant own a car:',
+        np.unique(copy_data['Car']))
+
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Realty = st.radio(
+        'Does the applicant own any Realty(properties):',
+        np.unique(copy_data['Realty']))
+
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Family_Status = st.radio(
+        'Is applicant Single/Married:',
+        np.unique(copy_data['Family_Status']))
+
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Income_Type = st.radio(
+        "What is the applicant's source of Income:",
+        np.unique(copy_data['Income_Type']))   
+
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_House_Type = st.radio(
+        "What is the applicant'type of House",
+        np.unique(copy_data['House_Type']))
+
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Education = st.radio(
+        'Education of the Applicant',
+        np.unique(copy_data['Education']))  
+
+input_Children = st.slider('Number of Children:', 0, max(copy_data["Children"]), 3)
+input_Family_Size = st.slider('Family Size:', 0, max(copy_data["Family_Size"]), 4)
+input_Salary = st.slider('Salary of the Applicant:', 0.0, max(copy_data["Salary"]), 85000.0)
+input_AGE = st.slider('Age of the applicant in Years:', 0.0, max(copy_data["AGE"]), 22.8)
+input_EXPERIENCE = st.slider('Experience of the applicant in Years:', 0.0, max(copy_data["EXPERIENCE"]), 7.5)
+input_ACCOUNT_DURATION = st.slider('Account Duration with the bank in Months:', 0, max(copy_data["ACCOUNT_DURATION"]), 18)
+
+
+# load model
+#best_xgboost_model = XGBClassifier()
+#best_xgboost_model.load_model("best_model.json")
+
 
 if st.button('Make Prediction'):
     inp_Income_Type = le1.inverse_transform(np.expand_dims(inp_Income_Type, -1))
